da4d7951aeb73bc28df52104c08cc552a3c8851e
mariam9498 <mm.bouhriz@gmail.com>
2021-02-07 22:34:22 +0100

    main files

--- src/main.js
+++ src/main.js
@@ -4,9 +4,16 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import Axios from "axios"
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 require('@/store/subscriber')
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Axios.defaults.baseURL = "http://localhost:8081/api"
 
 store.dispatch('auth/attempt', localStorage.getItem('token'))
