bde81875349cea16abd8468f8a05dd9b0138eafa
mariam9498 <mm.bouhriz@gmail.com>
2021-02-07 22:36:04 +0100

    add store time

---
+++ src/store/time.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export default {
+    namespaced: true,
+    state: {
+       times:[]
+    },
+    getters: {
+        GET_TIMES(state) {
+            return state.times;
+        }
+    },
+    mutations: {
+        SET_TIMES(state, times) {
+            state.times = times;
+        },
+    },
+
+    actions: {
+        async getTimes({ commit }) {
+            //axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
+            const res = await axios.get("/v1/times");
+            commit("SET_TIMES", res.data);
+        },
+        async createTime(time){
+            //axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
+            let response = await axios.post("/v1/times", time);
+            console.log(response);
+        }
+    }
+}
