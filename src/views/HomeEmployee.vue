692fa12a51b357ff7db96fea7850fb8e02698d2d
mariam9498 <mm.bouhriz@gmail.com>
2021-02-07 22:36:22 +0100

    add employee page

---
+++ src/views/HomeEmployee.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="homeEmployee">
+    <TimesCalendar/>
+  </div>
+</template>
+
+<script>
+import TimesCalendar from '@/components/user/TimesCalendar.vue';
+
+export default {
+name:"HomeEmployee",
+components:{
+    TimesCalendar
+}
+}
+</script>
+
+<style>
+</style>
