9d260365d5854978076ddb071dab9201f7ffc66a
mariam9498 <mm.bouhriz@gmail.com>
2021-02-09 00:16:55 +0100

    update store time

--- src/store/time.js
+++ src/store/time.js
@@ -14,6 +14,10 @@ export default {
         SET_TIMES(state, times) {
             state.times = times;
         },
+        DELETE_TIME(state, id){
+            let index = state.times.findIndex(time => time.id == id)
+            state.times.splice(index, 1)
+        }
     },
 
     actions: {
@@ -26,6 +30,14 @@ export default {
             //axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
             let response = await axios.post("/v1/times", time);
             console.log(response);
+            return dispatch("getTimes");
+        },
+        async deleteTime({dispatch}, id){
+            //console.log("this is the id" + id)
+            //axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
+            let response = await axios.delete(`/v1/times/${id}`);
+            console.log(response)
+            //commit("DELETE_TIME", id)
             return dispatch("getTimes");
         }
     }
