255e993afded49ad98f2e1b0236318a4063bc183
mariam9498 <mm.bouhriz@gmail.com>
2021-02-09 00:17:30 +0100

    Update Calender component

--- src/components/user/TimesCalendar.vue
+++ src/components/user/TimesCalendar.vue
@@ -1,58 +1,61 @@
 <template>
 <div>
     <div class="form-container">
-    <v-row id="edit" align="center" >
-        <v-btn x-large tile dark >
-        <v-icon left>mdi-pencil</v-icon> Edit Your Times
-        </v-btn>
-      </v-row>
-      <b-form @submit.prevent="onSubmit" novalidate>
+        <v-row id="edit" align="center" >
+            <v-btn id="update" x-large tile dark block v-on:click="showCalendar()">
+            <v-icon left large> mdi-calendar-multiselect</v-icon> Show  / Hide  my  calendar
+            </v-btn>
+        </v-row>
+        <v-row id="edit" align="center" >
+            <v-btn id="edit-btn" x-large tile block dark>
+            <v-icon left large>mdi-pencil</v-icon>  Edit  my  times
+            </v-btn>
+        </v-row>
+        <div id="editing">
+            <b-form @submit.prevent="onSubmit" novalidate>
+                <b-form-group id="input-group-dateStart" label="Start Date:" label-align="left">
+                    <b-form-input type="datetime-local" id="input-dateStart" v-model="form.dateStart" required placeholder="Enter dateStart"></b-form-input>
+                </b-form-group>
 
-        <b-form-group id="input-group-dateStart" label="Start Time:" label-align="left">
-            <b-form-input type="datetime-local" id="input-dateStart" v-model="form.dateStart" required placeholder="Enter dateStart"></b-form-input>
-        </b-form-group>
+                <b-form-group id="input-group-dateEnd" label="End Date:" label-align="left">
+                    <b-form-input type="datetime-local" id="input-dateEnd" v-model="form.dateEnd" required placeholder="Enter dateEnd"></b-form-input>
+                </b-form-group>
 
-        <b-form-group id="input-group-dateEnd" label="End Time:" label-align="left">
-            <b-form-input type="datetime-local" id="input-dateEnd" v-model="form.dateEnd" required placeholder="Enter dateEnd"></b-form-input>
-        </b-form-group>
+                <b-form-group id="input-group-projects" label="Projects:" label-align="left" >
+                <b-form-select  required v-model="form.project"   id="input-projects" >
+                    <option v-for="prj in projects" :key="prj.id" :value="prj.id">{{ prj.title }} : {{ prj.description }}</option>
+                </b-form-select>
+                </b-form-group>
 
-        <b-form-group id="input-group-projects" label="Projects:" label-align="left" >
-          <b-form-select  required v-model="form.project"   id="input-projects" >
-            <option v-for="prj in projects" :key="prj.id" :value="prj.id">{{ prj.title }} : {{ prj.description }}</option>
-          </b-form-select>
-        </b-form-group>
+                <b-button block type="submit" class="btn btn-poc" variant="dark">Add time</b-button>
+            </b-form><br>
+            
+            <b-form @submit.prevent="onSubmit" novalidate>
 
-        <b-button block type="submit" class="btn btn-poc" variant="dark">Add time</b-button>
-
-      </b-form>
-      <div id="delete" :key="componentKey">
-        <h6> Times list</h6>
-        <p>Select a time and click on "delete time" to delete it </p>
-        <v-card class="mx-auto" max-width="300" tile :key="componentKey">
-            <v-list dense  style="max-height: 70%" class="overflow-y-auto">
-            <v-subheader>Times</v-subheader>
-            <v-list-item-group v-model="selectedTime" color="primary">
-                <v-list-item v-for="item in times" :key="item.id">
-                <v-list-item-content> 
-                    <v-list-item-title>{{ item.project.title}} : <br>
-                        {{item.dateStart}} <br>
-                        {{item.dateEnd}}</v-list-item-title>
-                </v-list-item-content>
-                </v-list-item>
-            </v-list-item-group>
-             </v-list>
-        </v-card>
-        <b-button id="delbtn" block v-on:click="deleteTime(selectedTime)" class="btn btn-poc" variant="dark">Delete time</b-button>
-      </div>
+                <b-form-group id="input-group-times" label="Times list:" label-align="left" >
+                <b-form-select  required v-model="toDelete"   id="input-times" >
+                    <option v-for="t in times" :key="t.id" :value="t.id">{{ t.project.title }} ►  <br> {{ t.dateStart }} ► {{ t.dateEnd }} </option>
+                </b-form-select>
+                </b-form-group>
+                <p>Select a time and click on 'Delete time' to delete it </p>
+                <b-button id="delbtn" block v-on:click="deleteThisTime()" class="btn btn-poc" variant="dark">Delete time</b-button>
+            
+            </b-form>
+        </div><br>
+        <v-icon  x-large> mdi-lightbulb-on-outline</v-icon> Update Your Calendar After Editing It !
+        <v-row id="edit" align="center" >
+            <v-btn id="update" x-large tile block v-on:click="updateTimes()">
+            <v-icon left large> mdi-autorenew</v-icon>  Update  my  calendar
+            </v-btn>
+        </v-row>
     </div>
-    <div id= "mycalendar">
+    <div id= "mycalendar" v-if="seen_calendar" >
         <kalendar :configuration="calendar_settings" :events.sync="events" :key="componentKey">
         </kalendar>
     </div>
 </div>
-
-
 </template>
+
 <script>
 import Vue from "vue";
 import { mapActions, mapGetters } from "vuex";
@@ -77,12 +80,13 @@ Vue.use(Kalendar)
             projectId:'',
             username:'',
         },
+        toDelete: '',
         events:[],
         calendar_settings: {
         style: 'material_design',
         view_type: 'week',
         cell_height: 20,
-        scrollToNow: true,
+        scrollToNow: false,
         start_day: new Date().toISOString(),
         read_only: false,
         day_starts_at: 0,
@@ -92,8 +96,14 @@ Vue.use(Kalendar)
         hide_days: [7],
         past_event_creation: true,
         },
-        componentKey: 0
+        componentKey: 0,
+        seen_calendar: false,
     }),
+    created(){
+        this.getProjects();
+        this.getTimes();
+        this.updateTimes();
+    },
     computed: {
         ...mapGetters({
         projects: "project/GET_PROJECTS",
@@ -107,7 +117,12 @@ Vue.use(Kalendar)
         getProjects: "project/getProjects",
         getTimes: "time/getTimes",
         createTime: "time/createTime",
+        deleteTime: "time/deleteTime",
         }),
+        showCalendar(){
+            this.updateTimes();
+            this.seen_calendar=!this.seen_calendar;
+        },
 
         updateTimes(){
             this.events = [];
@@ -119,9 +134,8 @@ Vue.use(Kalendar)
                     data: time.project.title
                 },
             )
-        }
-        console.log(this.times)
-        this.componentKey += 1
+            }
+            this.componentKey += 1
         },
 
         onSubmit: function() {
@@ -129,24 +143,25 @@ Vue.use(Kalendar)
         this.timeRequest.dateEnd = this.form.dateEnd,
         this.timeRequest.projectId = this.form.project,
         this.timeRequest.username = this.user.username,
-        console.log(this.timeRequest)
         this.createTime(this.timeRequest)
-        this.updateTimes();
-        this.componentKey += 1
+        this.$swal( 'Click on "Update My Calendar!"',
+            'Time created!',
+            'success');
         },
-        deleteTime(selected){
-            console.log(selected)
-            this.events.splice(this.events.indexOf(selected), 1);
-            this.componentKey += 1
+
+        deleteThisTime(){
+            let idToDelete = this.toDelete
+            this.deleteTime(idToDelete)           
+            let index = this.times.findIndex(time => time.idToDelete == idToDelete)
+            this.times.splice(index, 1)
+            this.$swal( 'Click on "Update My Calendar!"',
+            'Time deleted!',
+            'success');
         }
     },
-    created(){
-        this.getProjects();
-        this.getTimes();
-        this.updateTimes();
-  }
   }
 </script>
+
 <style scoped>
 
 .form-container {
@@ -159,6 +174,17 @@ Vue.use(Kalendar)
     margin-bottom: 3%;
     margin-top: 3%;
 }
+#update{
+    margin-bottom: 3%;
+    margin-top: 3%;
+    color:white; 
+    background-color: rgb(31, 118, 225);
+}
+#edit-btn{
+
+    color: black;
+    background-color: rgb(240, 240, 240);
+}
 #delete{
     margin-bottom: 3%;
     margin-top: 3%;
@@ -166,6 +192,14 @@ Vue.use(Kalendar)
 #delbtn{
     margin-bottom: 3%;
     margin-top: 3%;
+}
+#mycalendar{
+    width : 70%;
+    float: right;
+}
+#editing{
+    background-color: rgb(240, 240, 240);
+    padding: 5%;
 }
 
 </style>
